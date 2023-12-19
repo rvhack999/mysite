372849f63146b6907d0a6ed25cf2f43f08d25691
rvhack <rvhack@yandex.ru>
2023-12-19 23:11:05 +0800

    create model User, Worker, Order add to admin.py

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Worker, Order
+
+
+# class UserAdmin(User):
 
 
 admin.site.register(User)
+admin.site.register(Worker)
+admin.site.register(Order)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,14 +1,51 @@
 from django.db import models
+from datetime import datetime
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20, help_text='Name')
+    """ Модель клиента """
+    name = models.CharField(max_length=20, help_text='Имя клиента')
     email = models.EmailField(help_text='email', null=True, blank=True)
-    phone = models.CharField(max_length=20, help_text='phone')
+    phone = models.CharField(max_length=20, help_text='Телефон клиента')
+    discount = models.FloatField(help_text='Скидка', blank=True, null=True, default=None)
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
     def __str__(self):
         return self.name
-# Create your models here.
+
+
+class Worker(models.Model):
+    """ Модель Мастеров и сотрудников """
+    name = models.CharField(max_length=50, help_text='Имя и фамилия сотрудника')
+    phone = models.CharField(max_length=20, help_text='Телефон сотрудника')
+    """ Нужно сделать список со специализациями мастера """
+    salary = models.FloatField(help_text='Процентная ставка')
+
+    class Meta:
+        verbose_name = 'Сторудник'
+        verbose_name_plural = 'Сторудники'
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=50, help_text='Заголовок')
+    category_mod = models.CharField(max_length=100, help_text='Модель оборудования', null=True, blank=True)
+    mod = models.TextField(help_text='Введите характер неисправности')
+    client_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    master_id = models.ForeignKey(Worker, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField(auto_created=True)
+    date_out = models.DateField(blank=True, null=True)
+    price = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+    def __str__(self):
+        return self.name
+
