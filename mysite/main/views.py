a7fc6c73026b57dd5fb9fd839bde4a5519429942
rvhack <rvhack@yandex.ru>
2023-12-26 23:30:32 +0800

    create html, install bootstrap

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 # Create your views here.
+def example(request):
+    return render(request, 'main/#')
